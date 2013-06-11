d072dd71cf517976e0a1be9de16c36f4bbfbbaad
conglp90 <conglp90@gmail.com>
2013-06-11 14:04:09 +0700

    1. clean

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,6 +6,12 @@ public class Calculator {
 
 	public static int add(String input) {
 		int kq=0;
+		if(input==null||input.isEmpty())
+			return 0;
+		kq=toInt(input);
 		return kq;
+	}
+	static int toInt(String input){
+		return Integer.parseInt(input);
 	}
 }
