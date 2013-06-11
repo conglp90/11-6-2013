fee15bb70d88b2da2ec685e2194dbc5bbad1033f
conglp90 <conglp90@gmail.com>
2013-06-11 14:12:43 +0700

    2.green

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,9 +6,18 @@ public class Calculator {
 
 	public static int add(String input) {
 		int kq=0;
+		ArrayList< Integer> numbers =new ArrayList<Integer>();
 		if(input==null||input.isEmpty())
 			return 0;
-		kq=toInt(input);
+		Matcher matcher=Pattern.compile("-?[0-9]+").matcher(input);
+		while (matcher.find()) {
+			int temp=toInt(matcher.group(0));
+			numbers.add(temp);
+		}
+		for (Integer temp : numbers) {
+			kq+=temp;
+		}
+		
 		return kq;
 	}
 	static int toInt(String input){
